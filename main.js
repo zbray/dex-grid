02c0c7c45053ee7c6d1ea181433cdcada09356df
Zach Bray <zachdbray@gmail.com>
2020-02-05 19:03:20 -0600

    begin work on js

---
+++ main.js
@@ -0,0 +1,14 @@
+const pokemon = [
+  {
+  number: 001,
+  name: "Bulbasaur",
+  type1: "grass",
+  type2: "poison",
+  imagesrc: "assets/gen-i/001Bulbasaur.png"
+
+}]
+
+
+
+
+//<div class="mon grass-poison"><img src="assets/gen-i/001Bulbasaur.png" alt="Bulbasaur"><br>001 Bulbasaur</div>
