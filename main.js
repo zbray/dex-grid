35797c74100682ad1e6507d6fcccd24214091f53
Zach Bray <zachdbray@gmail.com>
2023-07-12 19:35:23 -0400

    made images grey scale silhouette

--- main.js
+++ main.js
@@ -1,106 +1,106 @@
-const pokemon = [{
+const pokemon = [
+  {
     natdex: "001",
     name: "Bulbasaur",
     type1: "grass",
     type2: "poison",
-    imagesrc: "assets/gen-i/001Bulbasaur.png"
+    imagesrc: "assets/gen-i/001Bulbasaur.png",
   },
   {
     natdex: "001",
     name: "Ivysaur",
     type1: "",
     type2: "",
-    imagesrc: "assets/gen-i/002Ivysaur.png"
+    imagesrc: "assets/gen-i/002Ivysaur.png",
   },
   {
     natdex: "003",
     name: "Venusaur",
     type1: "",
     type2: "",
-    imagesrc: "assets/gen-i/003Venusaur.png"
+    imagesrc: "assets/gen-i/003Venusaur.png",
   },
   {
     natdex: "004",
     name: "Charmander",
     type1: "",
     type2: "",
-    imagesrc: "assets/gen-i/004Charmander.png"
+    imagesrc: "assets/gen-i/004Charmander.png",
   },
   {
     natdex: "005",
     name: "Charmeleon",
     type1: "",
     type2: "",
-    imagesrc: "assets/gen-i/005Charmeleon.png"
+    imagesrc: "assets/gen-i/005Charmeleon.png",
   },
   {
     natdex: "006",
     name: "Charizard",
     type1: "",
     type2: "",
-    imagesrc: "assets/gen-i/006Charizard.png"
+    imagesrc: "assets/gen-i/006Charizard.png",
   },
   {
     natdex: "007",
     name: "Squirtle",
     type1: "",
     type2: "",
-    imagesrc: "assets/gen-i/007Squirtle.png"
+    imagesrc: "assets/gen-i/007Squirtle.png",
   },
   {
     natdex: "008",
     name: "Wartortle",
     type1: "",
     type2: "",
-    imagesrc: "assets/gen-i/008Wartortle.png"
+    imagesrc: "assets/gen-i/008Wartortle.png",
   },
   {
     natdex: "009",
     name: "Blastoise",
     type1: "",
     type2: "",
-    imagesrc: "assets/gen-i/009Blastoise.png"
+    imagesrc: "assets/gen-i/009Blastoise.png",
   },
   {
     natdex: "010",
     name: "Caterpie",
     type1: "",
     type2: "",
-    imagesrc: "assets/gen-i/010Caterpie.png"
+    imagesrc: "assets/gen-i/010Caterpie.png",
   },
   {
     natdex: "011",
     name: "Metapod",
     type1: "",
     type2: "",
-    imagesrc: "assets/gen-i/011Metapod.png"
+    imagesrc: "assets/gen-i/011Metapod.png",
   },
   {
     natdex: "012",
     name: "Butterfree",
     type1: "",
     type2: "",
-    imagesrc: "assets/gen-i/012Butterfree.png"
+    imagesrc: "assets/gen-i/012Butterfree.png",
   },
   {
     natdex: "013",
     name: "Weedle",
     type1: "",
     type2: "",
-    imagesrc: "assets/gen-i/013Weedle.png"
+    imagesrc: "assets/gen-i/013Weedle.png",
   },
   {
     natdex: "014",
     name: "Kakuna",
     type1: "",
     type2: "",
-    imagesrc: "assets/gen-i/014Kakuna.png"
-  }
-]
+    imagesrc: "assets/gen-i/014Kakuna.png",
+  },
+];
 
+// for (var i = 0; i < pokemon.length; i++) {
+//   console.log(pokemon[i].natdex)
+// }
 
-for (var i = 0; i < pokemon.length; i++) {
-  console.log(pokemon[i].natdex)
-}
-
-//<div class="mon grass-poison"><img src="assets/gen-i/001Bulbasaur.png" alt="Bulbasaur"><br>001 Bulbasaur</div>
+{/* <div class="mon grass-poison"><img src="assets/gen-i/001Bulbasaur.png" alt="Bulbasaur"><br>001 Bulbasaur</div> */}
